79ecf5d1fb0085cc15387d90c1763a16a0ade2ff
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-06-12 17:11:39 +0100

    manager inquirer questions added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 // declare all packages here
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+// declare questions array
+const starterQuestions = [
+  {
+    type: "input",
+    message: "please enter your manager's name",
+    name: "managerName",
+    validate: (answer) => {
+      return answer ? true : "Please enter the manager's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter your manager ID:",
+    name: "managerID",
+    validate: (answer) => {
+      return answer ? true : "Please enter the manager's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter manager's email address:",
+    name: "managerEmailAdress",
+    validate: (answer) => {
+      return answer ? true : "Please enter the manager's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter manager's office number:",
+    name: "managerOfficeNumber",
+    validate: (answer) => {
+      return answer ? true : "Please enter the manager's details to continue";
+    },
+  },
+];
+
+// declare an init function
+const init = async () => {
+  const answers = await inquirer.prompt(starterQuestions);
+  console.log(answers);
+};
+// call the init function
+init();
+``;
