4e7adf01913d15c761ca08ca41e42ba41aca0029
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-06-12 20:08:33 +0100

    created array of enginner and intern qs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,79 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const getEngineerQuestions = () => {
+  console.log("success");
+};
+// create an arry of engineer questions
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "please enter your engineer's name",
+    name: "engineerName",
+    validate: (answer) => {
+      return answer ? true : "Please enter the engineer's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter your engineer ID:",
+    name: "engineerID",
+    validate: (answer) => {
+      return answer ? true : "Please enter the engineer's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter engineer's email address:",
+    name: "engineerEmailAdress",
+    validate: (answer) => {
+      return answer ? true : "Please enter the engineer's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter engineer's github username",
+    name: "engineerGithubUsername",
+    validate: (answer) => {
+      return answer ? true : "Please enter the engineer's details to continue";
+    },
+  },
+];
+// get an array of intern questions
+const internQuestions = [
+  {
+    type: "input",
+    message: "please enter your intern's name",
+    name: "internName",
+    validate: (answer) => {
+      return answer ? true : "Please enter the intern's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter your intern ID:",
+    name: "internID",
+    validate: (answer) => {
+      return answer ? true : "Please enter the manager's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter intern's email address:",
+    name: "internEmailAdress",
+    validate: (answer) => {
+      return answer ? true : "Please enter the intern's details to continue";
+    },
+  },
+  {
+    type: "input",
+    message: "Please enter intern's scholl",
+    name: "internSchool",
+    validate: (answer) => {
+      return answer ? true : "Please enter the intern's details to continue";
+    },
+  },
+];
 // declare questions array
 const starterQuestions = [
   {
@@ -36,13 +109,40 @@ const starterQuestions = [
       return answer ? true : "Please enter the manager's details to continue";
     },
   },
+  {
+    type: "list",
+    message: "Who would you like to add next?",
+    choices: [
+      {
+        name: "Add an engineer",
+        value: "engineer",
+        short: "engineer",
+        // when: (answer) => {
+        //   if (true) {
+        //     getEngineerQuestions();
+        //   }
+        // },
+      },
+      {
+        name: "Add an intern",
+        value: "intern",
+        short: "intern",
+      },
+      {
+        name: "Quit||no more employees needed",
+        value: "quit",
+        short: "quit",
+      },
+    ],
+    name: "addAnother",
+  },
 ];
 
 // declare an init function
 const init = async () => {
   const answers = await inquirer.prompt(starterQuestions);
+
   console.log(answers);
 };
 // call the init function
 init();
-``;
